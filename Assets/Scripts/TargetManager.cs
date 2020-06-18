8c077fc315945bfd0afadd0b5482b69d3ded73fe
lofell <icsorsa@gmail.com>
2020-06-18 15:44:30 +0900

    Project Upload

---
+++ Assets/Scripts/TargetManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetManager : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("isTargetShoted", false);
+        if (Input.GetKeyDown("1"))
+        {
+            anim.SetBool("isTargetShoted", true);
+        }
+    }
+}
